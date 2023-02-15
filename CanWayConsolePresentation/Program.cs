9a35e611e3b0d443005918cf2fedc914a8be5009
AaronL187 <72193157+AaronL187@users.noreply.github.com>
2023-02-15 11:30:48 +0100

    last version

--- CanWayConsolePresentation/Program.cs
+++ CanWayConsolePresentation/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConwayBusinessLogic;
-
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Kérem az oszlopok számát!");
 var columnsSting = Console.ReadLine();
@@ -17,6 +17,17 @@ if (Int32.TryParse(columnsSting, out columns)
     && Int32.TryParse(cellsSting, out cells))
 {
     var playArea = new PlayArea(columns,rows,cells);
+    var shouldContinue = true;
+    while (shouldContinue)
+    {
+        playArea.Randomize();
+        playArea.Draw();
+        var keyinfo = Console.ReadKey();
+        if (keyinfo.KeyChar=='x')
+        {
+            shouldContinue = false;
+        }
+    }
 }
 else
 {
--- ConwayBusinessLogic/PlayArea.cs
+++ ConwayBusinessLogic/PlayArea.cs
@@ -9,8 +9,8 @@ namespace ConwayBusinessLogic
     public class PlayArea
     {
         public int Columns { get; private set; }
-        public int Rows { get;private set ; }
-        public int Cells { get;private set; }
+        public int Rows { get; private set; }
+        public int Cells { get; private set; }
         bool[,] area;
         public PlayArea(int columns, int rows, int cells)
         {
@@ -20,14 +20,69 @@ namespace ConwayBusinessLogic
             area = new bool[Columns, Rows];
 
         }
-        private void Randomize()
+        public void Randomize()
         {
-
+            var rnd = new Random();
+            var elements = Enumerable.Range(0, Columns * Rows).ToHashSet();
+            var item = rnd.Next(0, elements.Count());
+            elements.Remove(item);
+            var itemCol = item / Columns;
+            var itemRow = item % Rows;
         }
         public void Draw()
         {
-
+            Console.Clear();
+            for (int c = 0; c < Columns; c++)
+            {
+                for (int r = 0; r < Rows; r++)
+                {
+                    Console.SetCursorPosition(c, r);
+                    if (area[c, r])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    char blackSquare = (char)9632;
+                    Console.Write(blackSquare);
+                }
+            }
+            Console.SetCursorPosition(0, 0);
 
         }
+        public void ApplyRules()
+        {
+            var tempArea= new bool[Columns, Rows];
+            for (int c = 0; c < Columns; c++)
+            {
+                for (int r = 0; r < Rows; r++)
+                {
+                  var neighbors = getNeighborCount(c,r);
+                }
+            }
+        }
+        private int getNeighborCount(int[,] matrix, int r, int c)
+        {
+            int count = 0;
+
+            // iterate through the neighboring elements
+            for (int i = Math.Max(r - 1, 0); i <= Math.Min(r + 1, matrix.GetLength(0) - 1); i++)
+            {
+                for (int j = Math.Max(c - 1, 0); j <= Math.Min(c + 1, matrix.GetLength(1) - 1); j++)
+                {
+                    // check if the neighboring element is not the current element
+                    if (i != r || j != c)
+                    {
+                        // increment the count for each neighboring element
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
     }
 }
