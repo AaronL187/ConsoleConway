79eba502a40840af3c257cbfa9d46ad022153d62
AaronL187 <72193157+AaronL187@users.noreply.github.com>
2023-02-15 10:19:22 +0100

    class

--- CanWayConsolePresentation/Program.cs
+++ CanWayConsolePresentation/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConwayBusinessLogic;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Kérem az oszlopok számát!");
 var columnsSting = Console.ReadLine();
@@ -7,15 +9,16 @@ var rowsSting = Console.ReadLine();
 Console.WriteLine("Kérem az elemek/sejtek számát!");
 var cellsSting = Console.ReadLine();
 
-
-try
+var columns = 0;
+var rows = 0;
+var cells = 0;
+if (Int32.TryParse(columnsSting, out columns)
+    && Int32.TryParse(rowsSting, out rows)
+    && Int32.TryParse(cellsSting, out cells))
 {
-    var columns = Convert.ToInt32(columnsSting);
-    var rows = Convert.ToInt32(rowsSting);
-    var cells = Convert.ToInt32(cellsSting);
+    var playArea = new PlayArea(columns,rows,cells);
 }
-catch (Exception ex)
+else
 {
-    Console.WriteLine("Nem megfelelő értékek!");
-	return;
+    Console.WriteLine("Nem megfelelő értékek");
 }
---
+++ ConwayBusinessLogic/PlayArea.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwayBusinessLogic
+{
+    public class PlayArea
+    {
+        public int Columns { get; private set; }
+        public int Rows { get;private set ; }
+        public int Cells { get;private set; }
+        bool[,] area;
+        public PlayArea(int columns, int rows, int cells)
+        {
+            Columns = columns;
+            Rows = rows;
+            Cells = cells;
+            area = new bool[Columns, Rows];
+
+        }
+        private void Randomize()
+        {
+
+        }
+        public void Draw()
+        {
+
+
+        }
+    }
+}
